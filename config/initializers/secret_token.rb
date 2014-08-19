241b5183b4d441a272ffb154f0c511afe494e31b
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2014-08-19 17:29:52 -0400

    Add an out of date gem.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foo::Application.config.secret_token = '6bc41a79f35da4e133643e2912f7b9006692023bc97f3f9f74ab28a9127789911defa98c293925e90d7c41a270536ab782ed0574d0f2ffed3671e4d85bf8f6be'
